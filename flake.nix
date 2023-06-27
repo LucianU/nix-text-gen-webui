f474187d7953784f719b373fd8c16da6cb068761
Lucian Ursu <lucian.ursu@gmail.com>
2023-06-27 12:10:17 +0300

    Add formatter and devShell with direnv setup
    Also, install nix-init in devShell to make it easier to create
    derivations.

--- flake.nix
+++ flake.nix
@@ -3,21 +3,52 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/23.05";
+
+    nix-init.url = "github:nix-community/nix-init";
+    nix-init.inputs.nixpkgs.follows = "nixpkgs";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix-init, treefmt-nix, ... }:
 
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
     in
-      {
-        packages = forAllSystems (system:
-          let
-            pkgs = nixpkgs.legacyPackages."${system}";
-          in
-            {
-              text-generation-webui = pkgs.python3.pkgs.callPackage ./package.nix { };
-            }
-        );
-      };
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
+          default = pkgs.python3.pkgs.callPackage ./package.nix { };
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              nix-init.packages.${system}.default
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        treefmt-nix.lib.mkWrapper
+          pkgs
+          {
+            projectRootFile = "flake.nix";
+            programs.nixpkgs-fmt.enable = true;
+          }
+      );
+    };
 }
